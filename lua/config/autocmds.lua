3e0da38812d376126c6923defd2df8d85f9c19ed
Stepan Fedorov <huanandeluxe@yandex.ru>
2024-06-06 16:28:34 +0300

    Update formatting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+  autocmd FileType typescript,typescriptreact,javascript,json setlocal tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+]])
